2f426704be8e93d4cab8b0db74b6b2bdbe17c051
slamsal <slamsal@go.olemiss.edu>
2018-10-03 14:41:11 -0500

    Radiobuttons work

--- src/pages/mode/mode.ts
+++ src/pages/mode/mode.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {  NavController } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import {  NavController, RadioButton } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 
 @Component({
@@ -7,10 +8,22 @@ import {  NavController } from 'ionic-angular';
   templateUrl: 'mode.html',
 })
 export class ModePage {
-
+ mode: string; 
+   
   constructor(public navCtrl: NavController) {
+    this.mode="teacher";
   }
-
+  
   
 
+  
+    continueToPage(){
+      if(this.mode=="teacher"){
+        this.navCtrl.push(HomePage);
+      }
+      else{
+        console.log("no");
+      }
+      
+    }
 }
