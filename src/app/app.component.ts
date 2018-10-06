49a80cbb74ec2fbe9d6d6bc9a81b5e21599e70d1
slamsal <slamsal@go.olemiss.edu>
2018-10-05 19:12:16 -0500

    Side menu has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,47 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform, Icon } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { LoginPage } from '../pages/login/login';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 import { ModePage } from '../pages/mode/mode';
+import { HomePage } from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  //rootPage:any = TabsPage;
-  // edit this after mode is created
+  
   rootPage:any = LoginPage;
-//public navCtrl: NavController
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  @ViewChild(Nav) nav: Nav;
+
+  pages: Array<{ title: string, component: any, icon: string}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    this.pages = [
+      {title: 'Logout', component:LoginPage, icon:'md-log-out'},
+      {title: 'About Us', component:AboutPage, icon:'md-eye'},
+      {title: 'Contact Us', component:ContactPage, icon:'md-contacts'},
+      {title: 'Home', component:HomePage, icon:'md-home'}
+    ];
+      
+      
+    
+    }
+    initializeApp(){
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // Here you can do any higher level native thinsgs you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
   }
-  
+  openPage(page){
+    this.nav.setRoot(page.component);
+  }
 }
